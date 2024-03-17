42a2862c5ba47cac4284507c58cef8afd4c1f3cc
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2024-03-16 20:18:51 -0500

    finish header style

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -13,24 +13,28 @@ export default function PageHeader() {
           <img src={logo} className="h-6" />
         </a>
       </div>
-      <div>
-        <form className="flex gap-4 flex-grow justify-center">
-          <div className="flex flex-grow max-w-[600px]">
-            <input
-              type="search"
-              placeholder="search"
-              className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full"
-            />
-            <Button>
-              <Search />
-            </Button>
-          </div>
-          <Button type="button" size="icon" className="flex-shrink-0">
-            <Mic />
+      <form className="md:flex hidden gap-4 flex-grow justify-center">
+        <div className="flex flex-grow max-w-[600px]">
+          <input
+            type="search"
+            placeholder="search"
+            className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full focus:border-blue-500 outline-none"
+          />
+          <Button className="py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0">
+            <Search />
           </Button>
-        </form>
-      </div>
+        </div>
+        <Button type="button" size="icon" className="flex-shrink-0">
+          <Mic />
+        </Button>
+      </form>
       <div className="flex flex-shrink-0 md:gap-2">
+        <Button size="icon" variant="ghost" className="md:hidden">
+          <Search />
+        </Button>
+        <Button size="icon" variant="ghost" className="md:hidden">
+          <Mic />
+        </Button>
         <Button size="icon" variant="ghost">
           <Upload />
         </Button>
