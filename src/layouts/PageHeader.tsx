33dc871a7cfa9b73125f13151e75b8a430e098af
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2024-03-16 20:08:58 -0500

    add header buttons, search bar

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,10 +1,10 @@
-import { Menu } from 'lucide-react'
+import { Bell, Menu, Mic, Search, Upload, User } from 'lucide-react'
 import logo from '../assets/Logo.png'
 import Button from '../components/Button'
 
 export default function PageHeader() {
   return (
-    <div className="flex gap-10 lg:gap-20 justify-between">
+    <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
       <div className="flex gap-4 items-center flex-shrink-0">
         <Button variant="ghost" size="icon">
           <Menu />
@@ -13,8 +13,34 @@ export default function PageHeader() {
           <img src={logo} className="h-6" />
         </a>
       </div>
-      <div></div>
-      <div></div>
+      <div>
+        <form className="flex gap-4 flex-grow justify-center">
+          <div className="flex flex-grow max-w-[600px]">
+            <input
+              type="search"
+              placeholder="search"
+              className="rounded-l-full border border-secondary-border shadow-inner shadow-secondary py-1 px-4 text-lg w-full"
+            />
+            <Button>
+              <Search />
+            </Button>
+          </div>
+          <Button type="button" size="icon" className="flex-shrink-0">
+            <Mic />
+          </Button>
+        </form>
+      </div>
+      <div className="flex flex-shrink-0 md:gap-2">
+        <Button size="icon" variant="ghost">
+          <Upload />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <Bell />
+        </Button>
+        <Button size="icon" variant="ghost">
+          <User />
+        </Button>
+      </div>
     </div>
   )
 }
