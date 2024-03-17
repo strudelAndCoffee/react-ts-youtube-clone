99076a4681cb308244cc00c2a3ad0f2b497843ef
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2024-03-16 19:47:18 -0500

    add button style

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,10 +1,14 @@
+import { Menu } from 'lucide-react'
 import logo from '../assets/Logo.png'
+import Button from '../components/Button'
 
 export default function PageHeader() {
   return (
     <div className="flex gap-10 lg:gap-20 justify-between">
       <div className="flex gap-4 items-center flex-shrink-0">
-        <button></button>
+        <Button variant="ghost" size="icon">
+          <Menu />
+        </Button>
         <a href="/">
           <img src={logo} className="h-6" />
         </a>
