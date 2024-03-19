4719e7e8d1cbe05cd63b02554f9711d85758cd6d
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2024-03-19 05:05:05 -0500

    finish sidebar style, open/close function

--- src/context/SidebarContext.tsx
+++ src/context/SidebarContext.tsx
@@ -1,4 +1,10 @@
-import { ReactNode, createContext, useState } from 'react'
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from 'react'
 
 type SidebarProviderProps = {
   children: ReactNode
@@ -13,9 +19,28 @@ type SidebarContextType = {
 
 const SidebarContext = createContext<SidebarContextType | null>(null)
 
+export function useSidebarContext() {
+  const value = useContext(SidebarContext)
+  if (value == null) throw Error('Cannot use outside of SidebarProvider')
+
+  return value
+}
+
 export function SidebarProvider({ children }: SidebarProviderProps) {
   const [isLargeOpen, setIsLargeOpen] = useState(true)
   const [isSmallOpen, setIsSmallOpen] = useState(false)
+
+  useEffect(() => {
+    const handler = () => {
+      if (!isScreenSmall()) setIsSmallOpen(false)
+    }
+
+    window.addEventListener('resize', handler)
+
+    return () => {
+      window.removeEventListener('resize', handler)
+    }
+  }, [])
 
   function isScreenSmall() {
     return window.innerWidth < 1024
--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -25,11 +25,19 @@ import { Children, ElementType, ReactNode, useState } from 'react'
 import Button, { buttonStyles } from '../components/Button'
 import { twMerge } from 'tailwind-merge'
 import { playlists, subscriptions } from '../data/sidebar'
+import { useSidebarContext } from '../context/SidebarContext'
+import { PageHeaderFirstSection } from './PageHeader'
 
 export default function Sidebar() {
+  const { isLargeOpen, isSmallOpen, close } = useSidebarContext()
+
   return (
     <>
-      <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+      <aside
+        className={`sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 ${
+          isLargeOpen ? 'lg:hidden' : 'lg:flex'
+        }`}
+      >
         <SmallSidebarItem Icon={Home} title="Home" url="/" />
         <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
         <SmallSidebarItem
@@ -39,7 +47,20 @@ export default function Sidebar() {
         />
         <SmallSidebarItem Icon={Library} title="Library" url="/library" />
       </aside>
-      <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 lg:flex hidden">
+      {isSmallOpen && (
+        <div
+          onClick={close}
+          className="lg:hidden fixed inset-0 z-[999] bg-secondary-dark opacity-50"
+        />
+      )}
+      <aside
+        className={`w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 ${
+          isLargeOpen ? 'lg:flex' : 'lg:hidden'
+        } ${isSmallOpen ? 'flex z-[999] bg-white max-h-screen' : 'hidden'}`}
+      >
+        <div className="lg:hidden pt-2 pb-4 px-2 sticky top-0 bg-white">
+          <PageHeaderFirstSection />
+        </div>
         <LargeSidebarSection>
           <LargeSidebarItem isActive Icon={Home} title="Home" url="/" />
           <LargeSidebarItem
