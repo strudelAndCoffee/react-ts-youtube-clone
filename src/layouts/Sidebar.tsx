0c53a5c77ac0620a6740bd4b34138cd0bfe53a46
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2024-03-19 04:27:55 -0500

    fix sidebar bug

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -39,7 +39,7 @@ export default function Sidebar() {
         />
         <SmallSidebarItem Icon={Library} title="Library" url="/library" />
       </aside>
-      <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 flex">
+      <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 lg:flex hidden">
         <LargeSidebarSection>
           <LargeSidebarItem isActive Icon={Home} title="Home" url="/" />
           <LargeSidebarItem
