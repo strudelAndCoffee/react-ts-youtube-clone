af6fd3aaa12f62e46ecdd33d171a8bc70060b073
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2024-03-19 02:58:50 -0500

    add video list, details, formatted numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import CategoryPills from './components/CategoryPills'
-import { categories } from './data/home'
+import { categories, videos } from './data/home'
 import PageHeader from './layouts/PageHeader'
+import VideoGridItem from './components/VideoGridItem'
 
 export default function App() {
   const [selectedCategory, setSelectedCategory] = useState(categories[0])
@@ -18,6 +19,11 @@ export default function App() {
               selectedCategory={selectedCategory}
               onSelect={setSelectedCategory}
             />
+          </div>
+          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px,1fr))]">
+            {videos.map((vid) => (
+              <VideoGridItem key={vid.id} {...vid} />
+            ))}
           </div>
         </div>
       </div>
