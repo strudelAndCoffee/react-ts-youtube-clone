0a8080be3e8017ccaabef1029cab6978b57f14f2
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2024-03-19 04:37:40 -0500

    create sidebar context

---
+++ src/context/SidebarContext.tsx
@@ -0,0 +1,52 @@
+import { ReactNode, createContext, useState } from 'react'
+
+type SidebarProviderProps = {
+  children: ReactNode
+}
+
+type SidebarContextType = {
+  isLargeOpen: boolean
+  isSmallOpen: boolean
+  toggle: () => void
+  close: () => void
+}
+
+const SidebarContext = createContext<SidebarContextType | null>(null)
+
+export function SidebarProvider({ children }: SidebarProviderProps) {
+  const [isLargeOpen, setIsLargeOpen] = useState(true)
+  const [isSmallOpen, setIsSmallOpen] = useState(false)
+
+  function isScreenSmall() {
+    return window.innerWidth < 1024
+  }
+
+  function toggle() {
+    if (isScreenSmall()) {
+      setIsSmallOpen((s) => !s)
+    } else {
+      setIsLargeOpen((l) => !l)
+    }
+  }
+
+  function close() {
+    if (isScreenSmall()) {
+      setIsSmallOpen(false)
+    } else {
+      setIsLargeOpen(false)
+    }
+  }
+
+  return (
+    <SidebarContext.Provider
+      value={{
+        isLargeOpen,
+        isSmallOpen,
+        toggle,
+        close,
+      }}
+    >
+      {children}
+    </SidebarContext.Provider>
+  )
+}
